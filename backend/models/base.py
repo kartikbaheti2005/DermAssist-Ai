a21c2d7391482fff7e547c39ed415dd692fc4b04
Kartikbaheti2005 <kartikpbaheti@gmail.com>
2026-06-22 00:57:16 +0530

    few updates in structure

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,1 +1,19 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+IST = ZoneInfo("Asia/Kolkata")
+
+
+def ist_now():
+    """
+    Returns current IST time.
+    Used as default timestamp for all database tables.
+    """
+    return datetime.now(IST)
